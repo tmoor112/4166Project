39044e25ed67ff6b30c3105612c0e4398ea5216b
Thomas Moore <tmoor112@uncc.edu>
2021-09-17 15:39:54 -0400

    init

---
+++ project 1/connection.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.getElementById("update").onclick = function () {
+        window.location.href = "connections.html";
+    }
+    document.getElementById("delete").onclick = function () {
+        window.location.href = "connections.html";
+    }
+    
+}
---
+++ project 1/newConnection.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    document.getElementById("create_new").onclick = function () {
+        window.location.href = "connections.html";
+    }
+}
